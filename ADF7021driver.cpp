33b240725a0c5d1a4beb6347b4bad08a984743ed
maisunmonowar <maisunmonwar@gmail.com>
2018-08-20 12:16:29 +0900

    setReg v4

--- ADF7021driver.cpp
+++ ADF7021driver.cpp
@@ -302,6 +302,19 @@ void callSign()
 {
 	J();	G();	M();	N();	B();
 }
+void readSiliconRevisionV2(){
+	setReg7();
+	readReg();
+	//calculate the value
+	revisionCode = 				8*db[4]+	4*db[3]+	2*db[2]+	db[1]		;
+	productCode = 	(100*(		8*db[16]+	4*db[15]+	2*db[14]+	db[13])) 	+
+					(10*(		8*db[12]+	4*db[11]+	2*db[10]+	db[9])) 	+
+					(1*	(		8*db[8]+	4*db[7]+	2*db[6]+	db[5]))		;
+	//show the user the value
+	cout <<"Revision code: "<< std::hex << "0x" << revisionCode << "     " << std::dec << revisionCode << endl;
+	cout <<"Product code : "<< std::hex << "0x" << productCode  << "   " << std::dec << productCode << endl;
+
+}
 int main(){
 	cout << "Main" << endl;
 	//pin direction and initial GPIO level
@@ -346,6 +359,7 @@ int main(){
 	tx_mode();
 	//hold for testing
 	readSiliconRevision();
+	readSiliconRevisionV2();
 	usleep(10000000);
 	//try call sign
 	space(); 	callSign(); 	space();	callSign();	space(); callSign();
