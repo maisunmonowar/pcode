fcbaa00f9e0698f15375f98883640a37826ca38e
Maisun Ibn Monowar <maisunmonwar@gmail.com>
2019-08-28 19:07:57 +0900

    ready to push via BBB

--- pyt/BBBBootloader.py
+++ pyt/BBBBootloader.py
@@ -1,12 +1,15 @@
+#import every thing necessary
 import serial
 import time
 import os
 import Adafruit_BBIO.GPIO as GPIO
 
+#define pins
 LED="P9_12"
 GPIO.setup(LED, GPIO.OUT)
 GPIO.output(LED, GPIO.LOW) #to enter bootloader mode
 
+#open Serial
 ser = serial.Serial()
 ser.port= "/dev/ttyO4"
 ser.baudrate = 9600
@@ -19,6 +22,7 @@ ser.rtscts = False
 ser.dsrdtr = False
 ser.writeTimeout = 5
 
+#prepare files
 nameOfFile = "appV2.hex"
 modeOfFile = "r"
 
@@ -34,9 +38,12 @@ try:
         ser.open()
 except Exception as e:
         print("error open Serial Port: " +str(e))
+        GPIO.output(LED, GPIO.HIGH) #Normally needs to be high
+        file.close()
+		GPIO.cleanup()
         exit()
 
-
+#do the work
 if ser.isOpen():
 	print("\n\rSerial is Open\n\r")
     for x in range(len(lineList)):
@@ -44,16 +51,20 @@ if ser.isOpen():
     		ser.write(lineList[x])
 else:
 	print("Serial is not open")
-GPIO.output(LED, GPIO.HIGH) #Normally needs to be high
 
-feedback = ser.read()
-print(feeback.decode())
-if feeback == 0x06:
-	print("Success")
+#analyze feedback
+for y in range(5):
+	feedback = ser.readline()
+	if (feeback.find(b'\x06') != -1):
+		print("Success feedback")
+
+#cleanup
+GPIO.output(LED, GPIO.HIGH) #Normally needs to be high
 ser.close()
 file.close()
 GPIO.cleanup()
 print("end")
+
 """import serial
 import time
 ser = serial.Serial('COM5', baudrate = 9600, timeout = 5, xonxoff=True)
