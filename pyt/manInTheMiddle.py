749a35c35b2d1059fb7aa6361254e7fbd41f602d
Maisun Ibn Monowar <maisunmonwar@gmail.com>
2019-09-11 17:07:57 +0900

    manIn the middle first test

--- pyt/manInTheMiddle.py
+++ pyt/manInTheMiddle.py
@@ -2,43 +2,43 @@
 import serial
 import time
 import os
-import Adafruit_BBIO.GPIO as GPIO
-
-#define pins
-LED="P9_12"
-GPIO.setup(LED, GPIO.OUT)
-GPIO.output(LED, GPIO.LOW) #to enter bootloader mode
-print("bootloader mode selected")
-feedback = "nothing"
 
 #open Serial
-UART4 = serial.Serial()
-UART4.port= "/dev/ttyO4"
-UART4.baudrate = 9600
-UART4.bytesize = serial.EIGHTBITS
-UART4.parity = serial.PARITY_NONE
-UART4.stopbits = serial.STOPBITS_ONE
-UART4.timeout = 1
-UART4.xonxoff= True
-UART4.rtscts = False
-UART4.dsrdtr = False
-UART4.writeTimeout = 1
+uart4 = serial.Serial()
+uart4.port= "/dev/ttyO4"
+uart4.baudrate = 9600
+uart4.bytesize = serial.EIGHTBITS
+uart4.parity = serial.PARITY_NONE
+uart4.stopbits = serial.STOPBITS_ONE
+uart4.timeout = 1
+uart4.xonxoff= True
+uart4.rtscts = False
+uart4.dsrdtr = False
+uart4.writeTimeout = 1
 
 uart1 = serial.Serial()
 uart1.port = "/dev/ttyO1"
-uart1
-#prepare files
-nameOfFile = "appv2.hex"
-modeOfFile = "r"
-
-with open(nameOfFile, modeOfFile) as file:
-    lineList = file.readlines()
-print("list loading done.\n\r")
-print("Number of lines: ")
-print(len(lineList))
+uart1.baudrate = 9600
+uart1.bytesize = serial.EIGHTBITS
+uart1.parity = serial.PARITY_NONE
+uart1.stopbits = serial.STOPBITS_ONE
+uart1.timeout = 1
+uart1.xonxoff= True
+uart1.rtscts = False
+uart1.dsrdtr = False
+uart1.writeTimeout = 1
 
 try:
-        ser.open()
+        uart4.open()
+except Exception as e:
+        print("error open Serial Port: " +str(e))
+        GPIO.output(LED, GPIO.HIGH) #Normally needs to be high
+        file.close()
+	GPIO.cleanup()
+        exit()
+
+try:
+        uart1.open()
 except Exception as e:
         print("error open Serial Port: " +str(e))
         GPIO.output(LED, GPIO.HIGH) #Normally needs to be high
@@ -47,61 +47,25 @@ except Exception as e:
         exit()
 
 #do the work
-if ser.isOpen():
-	#count start time
-	startTime = time.time()
-	print("\n\rSerial is Open\n\r")
-	#make sure the MCU is in bootloader mode:
-	while feedback.find(b'ownload') == -1:
-		feedback = ser.readline()
-		print("Feed, ")
-		print(feedback)
-		print("\n")
-	#flushing everything out of buffer
-	print(ser.readline())
-	print(ser.readline())
-	print(ser.readline())
-	print(ser.readline())
-	GPIO.output(LED, GPIO.HIGH)
+if uart4.isOpen() and uart1.isOpen():
+	uart4.write(b'a')
+	print("in waiting, " + str(uart1.in_waiting()))
+	print(uart1.readline())
 
-	for x in range(len(lineList)-2):
-    		print(lineList[x])
-    		ser.write(lineList[x])
-    		time.sleep(.1)
-		feedback = ser.readline()
-		print("Feed, ")
-    		print(feedback)
-    		#print("\n")
-		while (feedback.find(b'\x06') == -1):
-                	#means there was no feedback
-	                #try again
-			print("trying again")
-			ser.write("refresh\x0D")#im thinking serial line may be noisy. need to clear things???
-			ser.write(lineList[x])
-			time.sleep(.1)
-			feedback = ser.readline()
-			print("Feed, ")
-			print(feedback)
-	endTime = time.time()
-	#GPIO.output(LED, GPIO.HIGH)
-else:
-	print("Serial is not open")
+	uart4.write(b'b\x0D\x0A')
+	print("in waiting, " + str(uart1.in_waiting()))
+	print(uart1.readline())
 
-#analyze feedback
-for y in range(60):
-	feedback = ser.readline()
-	print(feedback)
-	if (feedback.find(b'\x06') != -1):
-		print("06 Received")
+	uart4.write("c")
+	print("in waiting, " + str(uart1.in_waiting()))
+	print(uart1.readline())
+
 
 #cleanup
-GPIO.output(LED, GPIO.HIGH) #Normally needs to be high
-ser.close()
+uart4.close()
+uart1.close()
 file.close()
 GPIO.cleanup()
 #count execution time
 #endTime = time.time()
-print("process took, ")
-print(endTime - startTime)
-print(" seconds\n\r")
 print("end")
