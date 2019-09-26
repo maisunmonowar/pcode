d242294ab8e4aca3be60244e889cd564e127965f
Maisun Ibn Monowar <maisunmonwar@gmail.com>
2019-09-26 17:15:43 +0900

    runtime bootload

---
+++ MP/blink.X/config.c
@@ -0,0 +1,10 @@
+#include <xc.h>
+
+#pragma config FOSC = HS
+#pragma config WDTE = OFF
+#pragma config PWRTE = OFF
+#pragma config BOREN = OFF
+#pragma config LVP = OFF
+#pragma config CPD = OFF
+#pragma config WRT = OFF
+#pragma config CP = OFF
---
+++ ccs/memoryOrg/memoryOrg.h
@@ -0,0 +1,6 @@
+#include <16F1787.h>
+#device ADC=16
+#use delay(crystal=8MHz)
+
+#define LED PIN_D0
+#define DELAY 1000
